922b6139a40cbaed1f162c7a6eb85881c6c8d697
Roméo <romeo.jacquemine@gmail.com>
2026-06-02 08:44:03 +0200

    ajout du modèle du rail droit

--- TD07/draw_scene.cpp
+++ TD07/draw_scene.cpp
@@ -1,4 +1,6 @@
 #include "draw_scene.hpp"
+#include "tools/basic_mesh.hpp"
+#include "tools/vector3d.hpp"
 
 /// Camera parameters
 float angle_theta {45.0};      // Angle between x axis and viewpoint
@@ -9,9 +11,16 @@ GLBI_Engine myEngine;
 
 IndexedMesh* cylinder;
 GLBI_Convex_2D_Shape cercle{3};
+GLBI_Convex_2D_Shape sides{3};
+GLBI_Convex_2D_Shape sides_mais{3};
 GLBI_Set_Of_Points x{3};
 GLBI_Set_Of_Points y{3};
 GLBI_Set_Of_Points z{3};
+
+float rr = 0.5;
+float sx = (2 - rr) / 2;
+float sr = 0.5;
+
 
 /***************************************** Déclaration formes gare *****************************************/
 IndexedMesh* cube;
@@ -58,15 +67,32 @@ void initCylinder(){
 	cercle.changeNature(GL_TRIANGLE_FAN);
 }
 
+void initRailCourbe(){
+	int nbPointDuCercle { 100 };
+	int rayon { 1 };
+	std::vector<float> initCercle {};
+	for(int i { 0 }; i < nbPointDuCercle; i++) {
+		float angle = 2 * M_PI * i / nbPointDuCercle;
+		initCercle.push_back(sin(angle) * rayon);
+		initCercle.push_back(0.0f);
+		initCercle.push_back(cos(angle) * rayon);
+	}
+
+	cercle.initShape(initCercle);
+	cercle.changeNature(GL_TRIANGLE_FAN);
+}
+
 void initScene() {
 	initAxes();
 	initCylinder();
+
+	cube = basicCube();
+	cube->createVAO();
 }
 
 void drawCylindreFerme() {
-	myEngine.setFlatColor(1.0, 0.0, 0.0);
+	
 	cercle.drawShape();
-	myEngine.setFlatColor(1.0, 1.0, 1.0);
 	cylinder->draw();
 
 	myEngine.mvMatrixStack.pushMatrix();
@@ -74,11 +100,96 @@ void drawCylindreFerme() {
 		myEngine.mvMatrixStack.addTranslation({ 0.0f, 1.f, 0.0f }); // Correction de la taille
 		myEngine.updateMvMatrix();
 
-		myEngine.setFlatColor(1.0, 0.0, 0.0); // Conversion des couleurs en [0, 1]
 		cercle.drawShape();
 
 	myEngine.mvMatrixStack.popMatrix();
 	myEngine.updateMvMatrix();
+}
+
+void drawRailDroit(){
+	myEngine.setFlatColor(142.0/255,142.0/255,142.0/255);
+	myEngine.mvMatrixStack.pushMatrix();
+			myEngine.mvMatrixStack.addTranslation({ 0.0, 0.0, rr/2});
+			myEngine.mvMatrixStack.pushMatrix();
+			myEngine.mvMatrixStack.addTranslation({ 3.0, 5.0, rr});
+			myEngine.updateMvMatrix();
+				myEngine.mvMatrixStack.pushMatrix();
+					myEngine.mvMatrixStack.addHomothety({ sr, 10.0, sr});
+					myEngine.updateMvMatrix();
+					cube->draw();
+			
+			myEngine.mvMatrixStack.popMatrix();
+			myEngine.updateMvMatrix();
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.updateMvMatrix();
+
+	myEngine.mvMatrixStack.pushMatrix();
+			myEngine.mvMatrixStack.addTranslation({ 7.0, 5.0, rr});
+			myEngine.updateMvMatrix();
+				myEngine.mvMatrixStack.pushMatrix();
+					myEngine.mvMatrixStack.addHomothety({ sr, 10.0, sr});
+					myEngine.updateMvMatrix();
+					cube->draw();
+			
+			myEngine.mvMatrixStack.popMatrix();
+			myEngine.updateMvMatrix();
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.updateMvMatrix();
+	
+
+	myEngine.setFlatColor(96.0/255,59.0/255,42.0/255);
+
+	myEngine.mvMatrixStack.pushMatrix();
+		Vector3D rt_1{0.0, 0.0, 1.0};
+		myEngine.mvMatrixStack.addRotation(-M_PI/2, rt_1);
+		myEngine.updateMvMatrix();
+
+
+
+		myEngine.mvMatrixStack.addTranslation({ -(sx + rr/2) , 2.0, 0.0});
+		myEngine.updateMvMatrix();
+
+		myEngine.mvMatrixStack.pushMatrix();
+
+		myEngine.mvMatrixStack.popMatrix();
+		myEngine.updateMvMatrix();
+
+
+		myEngine.mvMatrixStack.pushMatrix();
+			myEngine.mvMatrixStack.addHomothety({rr/2, 6.0, rr/2});
+			myEngine.updateMvMatrix();
+				drawCylindreFerme();
+		myEngine.mvMatrixStack.popMatrix();
+		myEngine.updateMvMatrix();
+
+		for (int i = 0; i < 4; i++)
+		{
+			myEngine.mvMatrixStack.addTranslation({ -(2*sx + rr) , 0.0, 0.0});
+			myEngine.updateMvMatrix();
+
+			myEngine.mvMatrixStack.pushMatrix();
+				myEngine.mvMatrixStack.addHomothety({rr/2, 6.0, rr/2});
+				myEngine.updateMvMatrix();
+					drawCylindreFerme();
+			myEngine.mvMatrixStack.popMatrix();
+			myEngine.updateMvMatrix();
+		}
+		
+
+		
+
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.updateMvMatrix();
+
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.updateMvMatrix();
+
+
+	
+}
+
+void drawRailCourbe(){
+
 }
 
 
@@ -92,34 +203,8 @@ void axes(){
 
 
 void drawScene() {
-/* 	glPointSize(10.0);
-
-	somePoints.drawSet();
-
-	myEngine.setFlatColor(0.2,0.0,0.0);
-	ground.drawShape();
- */
-	//Gare
-	/* myEngine.mvMatrixStack.pushMatrix();
-
-		myEngine.mvMatrixStack.addHomothety({20.f, 15.f, 10.f});
-
-		myEngine.updateMvMatrix();
-		myEngine.setFlatColor(124.0f/255.0f, 136.0f/255.0f, 132.0f/255.0f);
-		cube->draw();
-
-	myEngine.mvMatrixStack.popMatrix();
-	myEngine.updateMvMatrix(); */
-
-	//Fenêtre
-	myEngine.mvMatrixStack.pushMatrix();
-
-		myEngine.setFlatColor(124.0f/255.0f, 136.0f/255.0f, 132.0f/255.0f);
-		sphere->draw();
-
-	myEngine.mvMatrixStack.popMatrix();
-	myEngine.updateMvMatrix();
-
+	axes();
+	drawRailDroit();
 	
 }
 
